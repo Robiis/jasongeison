e7ed8fa0f2c2e1083b4ee84e840234bddcae9840
Robiis <59440340+Robiis@users.noreply.github.com>
2020-12-03 16:25:42 +0200

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -15,28 +15,32 @@ server.listen(process.env.PORT || 3000, function() {
     console.log("Server started");
 });
 
+let allIp = []
 vote1 = 0;
 vote2 = 0;
 
-setInterval(function() {
-    vote2 += 147;
-}, 1000);
-
 io.on("connection", function(socket) {
-    socket.on("answer", function(data) {
-        if (data === 1) {
-            vote1++;
-        } else if (data === 2) {
-            vote2++;
-        }
-        // fs.writeFile(path.join(__dirname, "data_file.txt"), "jasons:"+vote1.toString()+" geisons:"+vote2.toString(), function(err) {
-        //     if (err) throw err;
+    let ip = socket.handshake.address.address;
+    if (!allIp.includes(ip)) {
+        socket.on("answer", function(data) {
+            if (data === 1) {
+                vote1++;
+            } else if (data === 2) {
+                vote2++;
+            }
+            // fs.writeFile(path.join(__dirname, "data_file.txt"), "jasons:"+vote1.toString()+" geisons:"+vote2.toString(), function(err) {
+            //     if (err) throw err;
 
-        //     fs.readFile(path.join(__dirname, "data_file.txt"), "utf8" , (err, data) => {
-        //         if (err) throw err;
-        //         socket.emit("results", data);
-        //     });
-        // });
+            //     fs.readFile(path.join(__dirname, "data_file.txt"), "utf8" , (err, data) => {
+            //         if (err) throw err;
+            //         socket.emit("results", data);
+            //     });
+            // });
+            socket.emit("results", "jasons:"+vote1+" geisons:"+vote2);
+
+        });
+        allIp.push(ip);
+    } else {
         socket.emit("results", "jasons:"+vote1+" geisons:"+vote2);
-    });
+    }
 });
